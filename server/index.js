6028b82d93f062ac6b3d69c4d7f5e264baf928bc
abhijeetNmishra <geminiabhijeet@gmail.com>
2023-10-21 15:48:24 -0500

    New commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
+const colors = require('colors');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 8000;
 const app = express();
+
+//connect to database
+connectDB();
 
 //just a comment
 app.use('/graphql', graphqlHTTP({
